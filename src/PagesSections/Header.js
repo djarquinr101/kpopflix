7a0a6417d9b4ed9a7bb03ecaa40939457e80e7dc
djarquinr101 <daniel.jarquin4.dj@gmail.com>
2023-05-04 17:10:30 -0600

    styled-components was installed to styled the bootstrap components, and carousel was styled to make it responsive

--- src/PagesSections/Header.js
+++ src/PagesSections/Header.js
@@ -1,48 +1,77 @@
-import Carousel from 'react-bootstrap/Carousel';
+import { Card,Carousel, Button, Image } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+`
+
+const StyledCarousel = styled(Carousel)`
+  width: 100%;
+  height: auto;
+  max-height: 700px;
+  overflow: hidden;
+`
+const StyledItem = styled(Carousel.Item)`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%
+`
+
+const StyledImg = styled(Image)`
+  max-width: 100%;
+  max-height: 100%;
+  height: auto;
+  object-fit: contain;
+  `
 
 function CarouselFade() {
   return (
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
+    <StyledContainer>
+    <StyledCarousel fade>
+      <StyledItem>
+        <StyledImg 
           src="https://lh3.googleusercontent.com/EMI8vfiR_WWe4rywQxj-BW9yqwDr4P1c5a8X7AcfaWoafwf5SWzI8oQ5Qf_QE5OotO4XJST1cg3ifpUW2dhUdKocxV-KbFDs1Xyr1c5F6TdNixo=w1200-h600-pp-rj-s365"
-          alt="First slide"
+          alt="Blackpink"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
+      
+        <Carousel.Caption bg={'dark'}>
+          <h2>First slide label</h2>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Button variant={"light"}>Listen Now</Button>
         </Carousel.Caption>
-      </Carousel.Item>
+      </StyledItem>
 
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
+      <StyledItem>
+        <StyledImg
           src="https://cdn.shortpixel.ai/spai/q_lossy+w_844+h_515+to_auto+ret_img/https://koreanquiz.com/wp-content/uploads/2022/08/TWICE-Members-Quiz.jpg"
           alt="Second slide"
         />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption bg={'dark'}>
+          <h2>First slide label</h2>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Button variant={"light"}>Listen Now</Button>
         </Carousel.Caption>
-      </Carousel.Item>
+      </StyledItem>
       
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
+      <StyledItem>
+        <StyledImg
           src="https://stylecaster.com/wp-content/uploads/2020/08/itzy.jpg"
           alt="Third slide"
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+         <Carousel.Caption bg={'dark'}>
+          <h2>First slide label</h2>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Button variant={"light"}>Listen Now</Button>
         </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+        
+      </StyledItem>
+    </StyledCarousel>
+    </StyledContainer>
   );
-}
-
+  }
 export default CarouselFade;
