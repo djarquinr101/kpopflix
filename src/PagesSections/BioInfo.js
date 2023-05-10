196c6b38fff54417a0407e102528f94b041afb16
djarquinr101 <daniel.jarquin4.dj@gmail.com>
2023-05-10 17:11:32 -0600

    Style was added to the component that has the information of the band

--- src/PagesSections/BioInfo.js
+++ src/PagesSections/BioInfo.js
@@ -1,16 +1,27 @@
 import { Card } from "react-bootstrap";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 const StyledCard = styled(Card)`
-    margin-top: 2rem;
+    margin: 6rem 20% 5rem 20% ;
     padding: 2rem;
-`
+    border-radius: 32px;
+    box-shadow: 50px 50px 0px #5a5a5a,
+                -50px -50px 0px #E0E0E0;
+
+    ${ css`
+        @media (max-width: 820px) {
+        margin: 6rem 7% 6rem 7%;
+        box-shadow: 30px 30px 0px #5a5a5a,
+                -30px -30px 0px #E0E0E0;
+        }
+    `}                
+    `
 
 const InfoBio = ({name, infopart1, infopart2}) => {
 
     return (
         <>
-        <StyledCard bg={"secondary"}>
+        <StyledCard>
         <Card.Header>{name}</Card.Header>
         <Card.Body>
             <Card.Text>
